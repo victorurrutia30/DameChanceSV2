7907a00edaa01a44f9f752566d9c450f56c02a51
HUEZO URRUTIA VICTOR ELIAS <2748552016@mail.utec.edu.sv>
2025-03-21 22:30:12 -0600

    TEST

--- Program.cs
+++ Program.cs
@@ -24,7 +24,7 @@ namespace DameChanceSV2
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                // The default HSTS value is 30 days. YouK may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
